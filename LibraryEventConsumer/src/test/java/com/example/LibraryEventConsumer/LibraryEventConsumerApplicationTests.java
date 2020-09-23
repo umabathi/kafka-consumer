6193bbeab1004ebd6015e38da87476237e403c1d
Karuppusamy Arumugam <Karuppusamy Arumugam@Karuppu>
2020-09-23 19:26:45 +0530

    Kafka-consumer

---
+++ LibraryEventConsumer/src/test/java/com/example/LibraryEventConsumer/LibraryEventConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryEventConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
